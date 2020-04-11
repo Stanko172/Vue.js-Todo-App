1f75cc5db63bdcc135c9b4a4c70bf992a11768d9
Stanko Bebek <stanko.bebek82@gmail.com>
2020-04-11 13:49:43 +0200

    finished complete todo feature

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <Header />
-    <Todos />
+    <Todos 
+      :todos="todos"
+    />
   </div>
 </template>
 
@@ -14,6 +16,28 @@ export default {
   components: {
     Header,
     Todos
+    
+  },
+  data(){
+    return {
+      todos: [
+      {
+        id: 1,
+        title: 'Todo one',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Todo two',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Todo three',
+        completed: false
+      }
+    ]
+    }
     
   }
 }
--- src/components/todos/Todos.vue
+++ src/components/todos/Todos.vue
@@ -1,7 +1,15 @@
 <template>
     <div>
         <TodoMaker />
-        <h1>Todos</h1>
+            <div v-for="todo in todos" :key="todo.id" class="todo-item">
+                <div>
+                    <input type="checkbox" @change="changeComplete(todo)"/>
+                    <span :class="{'completed-todo': todo.completed}">
+                        {{todo.title}}
+                    </span>
+                </div>
+                <button type="submit" class="delete-button">X</button>
+            </div>
     </div>
 </template>
 
@@ -12,11 +20,42 @@ export default {
     name: 'Todos',
     components: {
         TodoMaker
+    },
+    props: [
+        'todos'
+    ],
+    methods: {
+        changeComplete(todo){
+            //ako prosljedimo id kao parametar
+            //const currentTodoIndex = this.todos.findIndex(x => x.id === id)
+            //const currentTodo = this.todos[currentTodoIndex] 
+            //currentTodo.completed = !currentTodo.completed
+            todo.completed = !todo.completed
+        }
     }
     
 }
 </script>
 
 <style scoped>
+    .todo-item{
+        display: grid;
+        grid-template-columns: 1fr auto;
+        background: #f4f4f4;
+        padding: 1em;
+        margin-bottom: 0.3em;
+    }
 
+    .delete-button{
+        background: red;
+        width: 1.5em;
+        border-radius: 50%;
+        color: white;
+        padding: 0.3em;
+        border: none;
+    }
+
+    .completed-todo{
+        text-decoration: line-through;
+    }
 </style>
